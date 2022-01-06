72c3e5138408105f781d4429f3d7c2129ca111a7
dimitraye <dimitraye94@gmail.com>
2022-01-06 13:06:25 +0100

    creation de la page qui liste tous les biens

--- src/app/component/list-properties/list-properties.component.ts
+++ src/app/component/list-properties/list-properties.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Property } from 'src/app/model/property.model';
+import { PropertyService } from 'src/app/service/property.service';
 
 @Component({
   selector: 'app-list-properties',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPropertiesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private propertyService : PropertyService) { }
+
+  properties! : Property[];
 
   ngOnInit(): void {
+    this.propertyService.getAll().subscribe(
+      data =>{
+        console.log('liste des biens' , data);
+        this.properties = data;
+      }
+    )
   }
 
 }
