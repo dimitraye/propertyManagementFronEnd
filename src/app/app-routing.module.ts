99a282b7eafb7ad93c213d094c7b393d93c187e2
dimitraye <dimitraye94@gmail.com>
2022-01-07 14:58:12 +0100

    Creation de la page individuelle d'un bien

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ListPropertiesComponent } from './component/list-properties/list-properties.component';
+import { ViewPropertyComponent } from './component/view-property/view-property.component';
 
 const routes: Routes = [
-  { path : 'properties' , component : ListPropertiesComponent}
+  { path : 'properties' , component : ListPropertiesComponent},
+  { path : 'properties/:id' , component : ViewPropertyComponent}
 ];
 
 @NgModule({
--- src/app/component/view-property/view-property.component.ts
+++ src/app/component/view-property/view-property.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Property } from '../../model/property.model';
+import { PropertyService } from '../../service/property.service';
 
 @Component({
   selector: 'app-view-property',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewPropertyComponent implements OnInit {
 
-  constructor() { }
+  property! : Property;
+
+  constructor(private propertyService : PropertyService,
+      private activatedRoute : ActivatedRoute,
+      private router : Router) { }
 
   ngOnInit(): void {
+    const id = this.activatedRoute.snapshot.params['id'];
+    this.propertyService.getOne(id).subscribe(data => {
+      console.log('id', id);
+      console.log('property', data);
+      this.property = data;
+    });
   }
 
 }
